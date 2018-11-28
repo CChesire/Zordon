c01813ddb6d936d4d515abb0e1c4320aa6c3cb76
Viers <krusnik.viers@gmail.com>
2018-11-28 21:06:58 +0300

    Hotfix for previous merge

--- app/handlers/context.py
+++ app/handlers/context.py
@@ -6,5 +6,11 @@ class Context:
         self.update = update
         self.bot = bot
 
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        pass
+
     def send_response_message(self, text, **kwargs) -> Message:
         return self.update.effective_chat.send_message(text, **kwargs)
