a2f08a4174eca9c83a9909aeee5c606f95d0468d
Viers <krusnik.viers@gmail.com>
2018-11-28 20:51:00 +0300

    Fix tests

--- tests/core/test_bot.py
+++ tests/core/test_bot.py
@@ -13,13 +13,3 @@ class TestLaunch(BaseTestCase):
         zordon_bot.updater.start_polling.assert_called_once_with()
         # zordon_bot.updater.dispatcher.add_handler.assert_called_with(MatcherAny())
         self.assertFalse(zordon_bot.updater.start_webhook.called)
-
-    @patch('app.core.bot.Updater', new=MagicMock())
-    @patch('sys.argv', ['_', '-w', 'http://test.url:1199'])
-    def test_webhook(self):
-        zordon_bot = app.core.bot.Bot()
-        zordon_bot.run()
-
-        zordon_bot.updater.start_webhook.assert_called_once_with(webhook_url='http://test.url:1199')
-        # zordon_bot.updater.dispatcher.add_handler.assert_called_with(MatcherAny())
-        self.assertFalse(zordon_bot.updater.start_polling.called)
